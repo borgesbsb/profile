72b2fff60a3c13dc9f97fe679e034a2288675719
Benjamin  da Silva Borges <borges.bnjamin@gmail.com>
2023-01-20 19:58:40 -0400

    add: changes profile

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,16 +2,15 @@
 
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to false if you don't want Splash screen.
+  isSplash: false, // Change this to false if you don't want Splash screen.
 };
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
-  description:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  title: "Benjamin Borges",
+  description: "Desenvolvedor de Sistemas Backend e Frontend",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Benjamin Borges",
     type: "website",
     url: "http://ashutoshhathidara.com/",
   },
@@ -19,11 +18,10 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
-  subTitle:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+  title: "Benjamin Borges",
+  logo_name: "Benjamin Borges",
+  nickname: "borgesbsb",
+  subTitle: "Desenvolvedor FullStack",
   resumeLink:
     "https://drive.google.com/file/d/1FL_wydnPF2SWCdw2goYAgZ1KKBx8TpcD/view?usp=sharing",
   portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
